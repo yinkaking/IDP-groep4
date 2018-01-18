dbf906877ea8c301441c92821f82518ac874f25f
Yinka Adetunji <yinka.adetunji@hva.nl>
2018-01-18 13:46:53 +0100

    Authentication toegevoegd!

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{\Illuminate\Foundation\Inspiring::quote()}}
                 </div>
 
                 <div class="links">
